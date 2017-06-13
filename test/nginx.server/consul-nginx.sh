5594abb4c214f24f876a25209371e61ad8af4aa8
johnnycao“TestCIgit config --global user.na.r.johnnycao“ <johnnycao@chinasystems.com>
2017-06-13 09:56:14 +0800

    122

--- test/nginx.server/consul-nginx.sh
+++ test/nginx.server/consul-nginx.sh
@@ -1,1 +1,1 @@
-consul-template -consul 10.39.101.226:8500  -template /AppConfig/test/nginx.server/nginx.tmpl:/AppConfig/test/nginx.server/nginx.conf:"docker restart nginx.server"
+consul-template -consul 10.39.101.226:8500  -template /cloud.config/test/nginx.server/nginx.tmpl:/cloud.config/test/nginx.server/nginx.conf:"docker restart nginx.server"
